188007151bfaf575fbd308cc2c81fbd139401982
Stine Richvoldsen <superrunt@gmail.com>
2014-05-14 12:32:03 -0600

    initial

---
+++ _posts/2014-05-07-angular-strap-date-range-directive.markdown
@@ -0,0 +1,23 @@
+---
+layout: post
+title:  "AngularJS date range handling"
+date:   2014-05-07 21:44:46
+categories: angularjs javascript bower
+short: "Date range directive for angular-strap datepicker. This directive lets you compare two angular-strap datepicker fields, for use when you have a to- and from-date in same scope."
+summary: "This directive lets you compare two angular-strap datepicker fields, for use when you have a to- and from-date in same scope. This directive observes the from-date. The datepicker minDate will always be set to the from-date + 1 day. If the from-date gets set to after the to-date, the to-date gets updated to from-date + 1 day. If the to-date is updated by the directive, the field 'flashes' to alert the user to this update. "
+---
+This directive lets you compare two angular-strap datepicker fields, for use when you have a to- and from-date in same scope.
+
+This directive observes the from-date. The datepicker minDate will always be set to the from-date + 1 day. If the from-date gets set to after the to-date, the to-date gets updated to from-date + 1 day. If the to-date is updated by the directive, the field 'flashes' to alert the user to this update.
+
+Easily implemented by adding laterthan="{{fromDate}}" to the to-date form field together with the bs-datepicker directive. If you would like to add a class to the form field that gets automatically updated, you can add it with changedclass="". I like to pulse the border in a different color. You can see an example of that in the example.html.
+
+{% highlight html %}
+<input type="text" class="form-control date" readonly="true" ng-model="searchParams.fromDate" data-min-date="today" bs-datepicker required />
+<input type="text" class="form-control date" readonly="true" ng-model="searchParams.toDate" data-min-date="''{{'toMinDate'}}'" laterthan="'{{'searchParams.fromDate'}}'" changedclass="changeAlert" bs-datepicker required />
+{% endhighlight %}
+
+Bower install:
+{% highlight bash %}
+bower install angular-strap-daterange
+ {% endhighlight %}
---
+++ js/main.js
@@ -0,0 +1,75 @@
+/**
+ * Created with RubyMine.
+ * By: superrunt
+ * Date: 5/8/14
+ * Time: 2:40 PM
+ */
+
+// MASONRY
+var transitionProp = getStyleProperty('transition');
+var transitionEndEvent = {
+    WebkitTransition: 'webkitTransitionEnd',
+    MozTransition: 'transitionend',
+    OTransition: 'otransitionend',
+    transition: 'transitionend'
+}[ transitionProp ];
+
+$( function() {
+    if ( $('.masonry-container').width() <= 535 ) { console.log("get outta here"); return; }
+
+    var $container = $('.masonry').masonry({
+        itemSelector: '.item',
+        columnWidth: '.grid-sizer'
+    });
+
+    $container.on( 'click', '.item-content', function( event ) {
+        var $this = $(this);
+
+        var previousContentSize = getSize( this );
+        // disable transition
+        this.style[ transitionProp ] = 'none';
+
+        if ( $this.find('div.post-summary-short').is(':visible') ) {
+            $this.find('div.post-summary-long').fadeIn();
+            $this.find('div.post-summary-short').fadeOut();
+        } else {
+            $this.find('div.post-summary-short').fadeIn();
+            $this.find('div.post-summary-long').fadeOut();
+        }
+
+        // set current size
+        $this.css({
+            width: previousContentSize.width,
+            height: previousContentSize.height
+        });
+
+        var $itemElem = $this.parent().toggleClass('is-expanded');
+
+        // force redraw
+        var redraw = this.offsetWidth;
+
+        // renable default transition
+        this.style[ transitionProp ] = '';
+
+        // reset 100%/100% sizing after transition end
+        if ( transitionProp ) {
+            var _this = this;
+            var onTransitionEnd = function() {
+                _this.style.width = '';
+                _this.style.height = '';
+            };
+            $this.one( transitionEndEvent, onTransitionEnd );
+        }
+
+        // set new size
+        var size = getSize( $itemElem[0] );
+        $this.css({
+            width: size.width,
+            height: size.height
+        });
+
+        $container.masonry();
+
+    });
+
+});
